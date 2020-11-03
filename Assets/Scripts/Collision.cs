e08ea7d2ff24aa24e832df9e915f88642c73545a
[hassanMetwally] <[has.metwally@gmail.com]>
2020-11-03 23:03:10 +0200

    full project

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    int score = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider c)
+    {
+
+        Debug.Log("trigger");
+
+        if (c.tag.Equals("coin"))
+        {
+            score++;
+
+            Destroy(c.transform.gameObject);
+
+        }
+
+
+    }
+}
